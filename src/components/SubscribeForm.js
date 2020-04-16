cd934bfadcca283de6d05553e2097d4ed5d8f1b3
mcb-11561 <m.boyle.91x@gmail.com>
2020-04-16 18:17:20 -0400

    add 'unfetch' npm

--- src/components/SubscribeForm.js
+++ src/components/SubscribeForm.js
@@ -1,13 +1,22 @@
-<form name="subscribeForm" method="POST" netlify-honeypot="bot-field" data-netlify="true" id="subscribe-form" class="subscribe-form">
-  <div class="screen-reader-text">
-    <label>Don't fill this out if you're human: <input name="bot-field" /></label>
-  </div>
-  <div class="form-row">
-    <label>
-      <span class="screen-reader-text">Email address</span>
-      <input class="subscribe-email" type="email" name="email" placeholder="Enter Email Address...">
-    </label>
-  </div>
-  <input type="hidden" name="form-name" value="subscribeForm" />
-  <button class="button" type="submit">Subscribe</button>
-</form><!-- .subscribe-form -->
+import React from 'react';
+
+export default class SubscribeForm extends React.Component {
+    render() {
+        return (
+            <form name="subscribeForm" method="POST" netlifyHoneypot="bot-field" data-netlify="true" id="subscribe-form"
+              className="subscribe-form">
+              <div className="screen-reader-text">
+                <label>Don't fill this out if you're human: <input name="bot-field" /></label>
+              </div>
+              <div className="form-row">
+                <label>
+                  <span className="screen-reader-text">Email address</span>
+                  <input className="subscribe-email" type="email" name="email" placeholder="Enter Email Address..."/>
+                </label>
+              </div>
+              <input type="hidden" name="form-name" value="subscribeForm" />
+              <button className="button" type="submit">Subscribe</button>
+            </form>
+        );
+    }
+}
