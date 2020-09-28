62488548cbd2dcc33dc6a65427cb475a0c9c434e
mcb-11561 <m.boyle.91x@gmail.com>
2020-09-27 21:48:27 -0400

    TransferForm(iii)

--- src/components/TransferForm/Step1.js
+++ src/components/TransferForm/Step1.js
@@ -1,52 +1,42 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 import {useForm} from 'react-hook-form';
+import { navigate } from 'gatsby';
 import {CardElement, useStripe, useElements} from '@stripe/react-stripe-js';
-import {navigate} from 'gatsby';
 import {placeOrder} from './api';
 
 import Field from './Field';
 
-function Step1({state, onComplete}) {
-  const {company} = state;
+function encode(data) {
+    return Object.keys(data)
+      .map((key) => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
+      .join('&')
+  }
+  
+  export default function Step1() {
+    const [state, setState] = React.useState({})
 
   const stripe = useStripe();
   const elements = useElements();
 
-  const [stripeError, setStripeError] = useState();
-  const [error, setError] = useState();
-
-  const {
-    handleSubmit,
-    register,
-    control,
-    errors,
-    setValue,
-    getValues,
-    watch,
-    formState: {isSubmitting, isSubmitted, isValid},
-  } = useForm({
-    mode: 'onChange',
-    defaultValues: {},
-  });
-
-  const onSubmit = async (values) => {
-    setError();
-    setStripeError();
-
-    if (!stripe || !elements) {
-      // Stripe.js has not loaded yet. Make sure to disable
-      // form submission until Stripe.js has loaded.
-      return;
-    }
-
-    function encode(data) {
-      return Object.keys(data)
-        .map((key) => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
-        .join('&')
-    }
-
+    const {
+      register,
+      control,
+      errors,
+      setValue,
+      getValues,
+      watch,
+      formState: {isSubmitting, isSubmitted, isValid},
+    } = useForm({
+      mode: 'onChange',
+      defaultValues: {},
+    });
+  
+    const handleChange = (e) => {
+      setState({ ...state, [e.target.name]: e.target.value })
+    };
+  
     const handleSubmit = (e) => {
       e.preventDefault()
       const form = e.target
@@ -61,88 +51,40 @@ function Step1({state, onComplete}) {
         .then(() => navigate(form.getAttribute('action')))
         .catch((error) => alert(error))
     }
-
-    const {paymentMethod, error: newStripeError} = await stripe.createPaymentMethod({
-      type: 'card',
-      card: elements.getElement(CardElement),
-      billing_details: {
-        name: `${values.firstName} ${values.lastName}`,
-        email: values.email,
-      },
-    });
-
-    if (newStripeError) {
-      setStripeError(newStripeError);
-      elements.getElement('card').focus();
-      return Promise.resolve({success: false});
-    }
-
-    const payload = {
-      ...state,
-      ...values,
-      paymentMethod: paymentMethod.id,
-    };
-
-  return (
-    <form name="transferLEI" action="/thanks/" id="transfer-LEI" className="transfer-LEI" method="post" action="/thanks/" data-netlify="true" netlify-honeypot="bot-field" onSubmit={handleSubmit(onSubmit)}>
-        <input type="hidden" name="form-name" value="transferLEI" />
-
-      <div className="screen-reader-text">
-                    <label>
-                        Don't fill this out if you're human: <input name="bot-field" />
-                    </label>
-                </div>
-
-            <section>
-        <h3>Company Information</h3>
-
-        <Field
-          name="Company Name"
-          label="Company Name"
-          ref={register({
-            required: true,
-          })}
-        />
-
-        <Field
-          name="LEI Code"
-          label="LEI Code"
-          ref={register({
-            required: true,
-          })}
-        />
-      </section>
-
-      <section>
-        <h3>Applicant Information</h3>
-
-        <Field
-          name="Name"
-          label="Name"
-          ref={register({
-            required: true,
-          })}
-        />
-
-        <Field
-          name="Position"
-          label="Position"
-          ref={register({
-            required: true,
-          })}
-        />
-
-        <Field
-          name="email"
-          label="Email"
-          type="email"
-          ref={register({
-            required: true,
-          })}
-        />
-      </section>
-
-      <section>
+  
+        return (
+      <form name="transfer" method="post" action="/thanks/" data-netlify="true" data-netlify-honeypot="bot-field"
+        onSubmit={handleSubmit}
+      >
+        {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
+        <input type="hidden" name="form-name" value="transfer" />
+        <p hidden>
+          <label>
+            Don’t fill this out: <input name="bot-field" onChange={handleChange} />
+          </label>
+        </p>
+        <p>
+          <label>
+            Your name:
+            <br />
+            <input type="text" name="name" onChange={handleChange} />
+          </label>
+        </p>
+        <p>
+          <label>
+            Your email:
+            <br />
+            <input type="email" name="email" onChange={handleChange} />
+          </label>
+        </p>
+        <p>
+          <label>
+            Message:
+            <br />
+            <textarea name="message" onChange={handleChange} />
+          </label>
+        </p>
+        <section>
         <h3>Payment Method</h3>
         <Field>
           <CardElement />
@@ -161,30 +103,14 @@ function Step1({state, onComplete}) {
                 required: true,
               })}
             />{' '}
-            I hereby accept the Terms & Conditions and Privacy Policy and give permission to transfer
-            my LEI.
+            I hereby accept the Terms & Conditions and Privacy Policy and give permission to apply
+            for an LEI.
           </label>
         </Field>
       </section>
-
-      {/* {isValid && ( */}
-      <div>
-        <button type="submit" disabled={isSubmitting}>
-          {isSubmitting ? 'Submitting...' : 'Submit'}
-        </button>
-      </div>
-      {/* )} */}
-
-      {error && (
-        <div style={{marginTop: '1rem', color: 'red'}}>
-          {error.failureReason ||
-            (error.type?.startsWith('StripeCardError') && 'There was an error with your card.')}
-        </div>
-      )}
-
-      {stripeError && <div style={{marginTop: '1rem', color: 'red'}}>{stripeError.message}</div>}
-    </form>
-  );
-}
-
-export default Step1;
+        <p>
+          <button type="submit">Send</button>
+        </p>
+      </form>
+        );
+    }
