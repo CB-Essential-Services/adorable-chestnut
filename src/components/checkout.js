bbd94f5b26df8564c193916ed7fa827fcec7113b
mcb-11561 <m.boyle.91x@gmail.com>
2020-10-01 23:12:43 -0400

    Another attempt: Get button to both submit form data to Netlify and create a Stripe charge

--- src/components/checkout.js
+++ src/components/checkout.js
@@ -1,28 +1,11 @@
 import React, { useState } from "react";
 import { loadStripe } from "@stripe/stripe-js";
-import { navigate } from 'gatsby-link'
 
 function encode(data) {
     return Object.keys(data)
       .map((key) => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
       .join('&')
   }
-
-const buttonStyles = {
-  fontSize: "13px",
-  textAlign: "center",
-  color: "#000",
-  padding: "12px 60px",
-  boxShadow: "2px 5px 10px rgba(0,0,0,.1)",
-  backgroundColor: "rgb(255, 178, 56)",
-  borderRadius: "6px",
-  letterSpacing: "1.5px",
-}
-
-const buttonDisabledStyles = {
-  opacity: "0.5",
-  cursor: "not-allowed",
-}
 
 let stripePromise
 const getStripe = () => {
@@ -53,43 +36,34 @@ const Checkout = () => {
     }
   }
 
-  const [state, setState] = React.useState({})
-
-  const handleChange = (e) => {
-    setState({ ...state, [e.target.name]: e.target.value })
-  }
-
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    const form = e.target
-    fetch('/', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-      body: encode({
-        'form-name': form.getAttribute('name'),
-        ...state,
-      }),
-    })
-      .then(() => navigate(form.getAttribute('action')))
-      .catch((error) => alert(error))
-  }
-
-  return (
-<form name="transferlei" method="POST" data-netlify="true" id="transfer-lei" class="transferlei">
-    <p class="form-row">
-        <label id="transfer-lei-name-label" for="transfer-lei-name" class="form-label">Name</label>
-        <input type="text" name="name" id="transfer-lei-name" aria-labelledby="transfer-lei-name-label" class="form-input" />
+  return (    
+<form
+    name="transfer"
+    method="POST"
+    action="/thanks"
+    data-netlify-honeypot="bot-field"
+    data-netlify="true"
+    id="transfer"
+    className="transfer"
+>
+    <p className="screen-reader-text">
+        <label>Don't fill this out if you're human: <input name="bot-field" /></label>
     </p>
-    <p class="form-row">
-        <label id="transfer-lei-email-label" for="transfer-lei-email" class="form-label">Email address</label>
-        <input type="email" name="email" id="transfer-lei-email" aria-labelledby="transfer-lei-email-label" class="form-input" />
+    <p className="form-row">
+        <label htmlFor="transfer-name" className="form-label">Name</label>
+        <input type="text" name="name" id="transfer-name" className="form-input" />
     </p>
-    <p class="form-row form-submit">
-        <button type="submit" class="button" disabled={loading}
-      style={
-        loading ? { ...buttonStyles, ...buttonDisabledStyles } : buttonStyles
-      }
-      onClick={redirectToCheckout}>Pay</button>
+    <p className="form-row">
+        <label htmlFor="transfer-email" className="form-label">Email address</label>
+        <input type="email" name="email" id="transfer-email" className="form-input" />
+    </p>
+    <input type="hidden" name="form-name" value="transfer" />
+    <p className="form-row form-submit">
+        <button type="submit" className="button" 
+        disabled={loading}
+      onClick={redirectToCheckout}>
+          Pay
+          </button>
     </p>
 </form>
   )
