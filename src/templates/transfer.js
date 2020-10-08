e207604025470b4fc958692e9a9cf6d237f0d0fd
mcb-11561 <m.boyle.91x@gmail.com>
2020-10-08 13:28:45 -0400

    Add JS to transfer in attempt 2 get Netlify submit

--- src/templates/transfer.js
+++ src/templates/transfer.js
@@ -1,9 +1,46 @@
 import React from 'react';
-import Checkout from '../components/checkout';
 
 import {Layout} from '../components/index';
+import Checkout from '../components/checkout';
 
 export default class Landing extends React.Component {
+    state = {
+        name: '',
+        email: '',
+        message: ''
+      };
+      handleInputChange = (event) => {
+        const target = event.target;
+        const value = target.value;
+        const name = target.name;
+        this.setState({
+            [name]: value
+        });
+      };
+      encode = (data) => {
+        return Object.keys(data)
+            .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+            .join("&");
+      }
+      handleSubmit = async (e) => {
+        e.preventDefault();
+        const options = {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+          body: this.encode({ 'form-name': 'contactForm', ...this.state })
+        }
+  
+        fetch(
+          "/",
+          options
+        )
+        .then(function (response) {
+          window.location.assign('/thanks/');
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+      };
     render() {
         return (
             <Layout {...this.props}>
